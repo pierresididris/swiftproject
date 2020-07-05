9515c48c92a91a1f81b1720fff1bdc6ee3937c07
Pierre Sid-idris <lucieoliveira@MacBook-Pro-de-Lucie.local>
2020-07-05 11:26:31 +0200

    refactor sport model

--- SwiftPodcast/ContentView.swift
+++ SwiftPodcast/ContentView.swift
@@ -8,47 +8,47 @@
 
 import SwiftUI
 
-struct Country: Identifiable {
+struct SportType: Identifiable {
     var id: Int = 0
     var name: String
-    var cities: [City]
+    var sports: [Sport]
 }
 
-struct City: Identifiable {
+struct Sport: Identifiable {
     var id: Int = 0
     var name: String
 }
 
 class Model: ObservableObject {
-    let countries: [Country] = [
-        Country(id: 0, name: "Individuel", cities: [
-            City(id: 0, name: "judo"),City(id: 1, name: "boxe"),City(id: 2, name: "karate"),City(id: 3, name: "tir à l'arc")
+    let sportPodcast: [SportType] = [
+        SportType(id: 0, name: "Individuel", sports: [
+            Sport(id: 0, name: "judo"),Sport(id: 1, name: "boxe"),Sport(id: 2, name: "karate"),Sport(id: 3, name: "tir à l'arc")
         ]),
-        Country(id: 1, name: "Collectifs", cities: [
-            City(id: 0, name: "football"),City(id: 1, name: "handball"),City(id: 2, name: "rugby"),City(id: 3, name: "voleyball")
+        SportType(id: 1, name: "Collectifs", sports: [
+            Sport(id: 0, name: "football"),Sport(id: 1, name: "handball"),Sport(id: 2, name: "rugby"),Sport(id: 3, name: "voleyball")
         ])
     ]
 
-    @Published var selectedContry: Int = 0 {
+    @Published var selectedType: Int = 0 {
         willSet {
-            selectedCity = 0
+            selectedSport = 0
             id = UUID()
             print("country changed")
         }
     }
     @Published var id: UUID = UUID()
-    @Published var selectedCity: Int = 0
-    var countryNemes: [String] {
-        countries.map { (country) in
-            country.name
+    @Published var selectedSport: Int = 0
+    var sportTypeNames: [String] {
+        sportPodcast.map { (sportType) in
+            sportType.name
         }
     }
-    var cityNamesCount: Int {
-        cityNames.count
+    var sportNamesCount: Int {
+        sportNames.count
     }
-    var cityNames: [String] {
-        countries[selectedContry].cities.map { (city) in
-            city.name
+    var sportNames: [String] {
+        sportPodcast[selectedType].sports.map { (sport) in
+            sport.name
         }
     }
 }
@@ -60,17 +60,17 @@ struct ContentView: View {
         return VStack {
             Text("Bonjour User ")
             Section(header: Text("Choisissez un type de sport")) {
-              Picker(selection: $model.selectedContry, label: Text("")){
-                  ForEach(0 ..< model.countryNemes.count){ index in
-                      Text(self.model.countryNemes[index])
+              Picker(selection: $model.selectedType, label: Text("")){
+                  ForEach(0 ..< model.sportTypeNames.count){ index in
+                      Text(self.model.sportTypeNames[index])
                   }
               }.labelsHidden()
               .clipped()
             }
             Section(header: Text("Choisissez le sport qui vous interresse")) {
-              Picker(selection: $model.selectedCity, label: Text("")){
-                  ForEach(0 ..< model.cityNamesCount){ index in
-                      Text(self.model.cityNames[index])
+              Picker(selection: $model.selectedSport, label: Text("")){
+                  ForEach(0 ..< model.sportNamesCount){ index in
+                      Text(self.model.sportNames[index])
                   }
               }
               // !! changing views id force SwiftUI to recreate it !!
