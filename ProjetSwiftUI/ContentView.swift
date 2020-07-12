e25c04d42e1896b8efa770fb8716c74e83406cbe
Pierre Sid-idris <lucieoliveira@MacBook-Pro-de-Lucie.local>
2020-07-12 22:20:49 +0200

    add test feature

--- ProjetSwiftUI/ContentView.swift
+++ ProjetSwiftUI/ContentView.swift
@@ -9,110 +9,137 @@
 import SwiftUI
 
 struct ContentView:View {
-
+    
     @State private var sportCategoryIndex = 0;
-
     var sportsCategories = ["Physique", "Mental", "Vehicule", "Coordination", "Animaux"];
+    
+    @State private var sportCollIndvIndex = 0;
+    var sportsCollIndv = ["Collectif", "Individuel"];
 
+    
     @State var firstname: String = ""
     @State var lastname: String = ""
     @State var description: String = ""
-
+    
     
     @State var isShowPicker: Bool = false
     @State var image: Image? = Image("placeholder")
-
+    
+    @State var height : CGFloat? //current height
+    
+    let constHeightRatio : CGFloat = 0.55 //use for assembly with other fonts.
+    let defaultHeight : CGFloat = 250 //use for assembly with other views
     
     var body: some View {
-
+        
         TabView{
             NavigationView{
-                    VStack{
-                        Picker(selection: $sportCategoryIndex, label: Text("")){
-                            ForEach(0 ..< sportsCategories.count){
-                                Text(self.sportsCategories[$0]).tag($0)
-                            }
-                        }.labelsHidden()
-                        
-                        NavigationLink(destination: ResultView(choice: "\(sportCategoryIndex)")){
-                            Text("Next >")
-                        }.padding(100)
-                    }
-                    .navigationBarTitle("Trouver votre podcast")
+                VStack{
+                    Picker(selection: $sportCategoryIndex, label: Text("")){
+                        ForEach(0 ..< sportsCategories.count){
+                            Text(self.sportsCategories[$0]).tag($0)
+                        }
+                    }.labelsHidden()
+                    
+                    NavigationLink(destination: ResultView(choice: "\(sportCategoryIndex)")){
+                        Text("Next >")
+                    }.padding(100)
+                }
+                .navigationBarTitle("Trouver votre podcast")
                 
-
             }.tabItem {
-            Image(systemName: "doc.text.magnifyingglass")
-            Text("consulter")
-        }
+                Image(systemName: "doc.text.magnifyingglass")
+                Text("consulter")
+            }
             NavigationView{
                 Form {
-                   Section(header: Text("Autheur")) {
-                    TextField("Nom", text: $firstname)
-                    TextField("Prénom", text: $lastname)
-
-                   }
-                   
-                   Section(header: Text("Podcast")) {
-//                    TextField("Description du podcast", text: $description)
-                    MultilineTextView(text: $description)
-                       ZStack {
-                        VStack(alignment: .center) {
-                               image?
-                                   .resizable()
-                                   .scaledToFit()
-                                   .frame(height: 150)
-                               Button(action: {
-                                   withAnimation {
-                                       self.isShowPicker.toggle()
-                                   }
-                               }) {
-                                   Image(systemName: "photo")
-                                       .font(.headline)
-                                Text("importer").font(.headline).foregroundColor(Color.blue).multilineTextAlignment(.center).lineLimit(nil).padding()
-                               }.foregroundColor(.black)
-                               Spacer()
-                           }
-                       }
-                       .sheet(isPresented: $isShowPicker) {
-                           ImagePicker(image: self.$image)
-                       }
-                   }
-                   
-                   Section {
-                       Button(action: {
-                           print("Perform an action here...")
-                       }) {
-                           Text("Partager le podcast")
-                       }
-                   }
-               }
-               .navigationBarTitle("Proposer un podcast")
+                    Section(header: Text("Autheur")) {
+                        TextField("Nom", text: $firstname)
+                        TextField("Prénom", text: $lastname)
+                        
+                    }
+                    
+                    Section(header: Text("description")) {
+                        MultilineTextView(text: $description)
+                    }
+                    Section(header: Text("Categorie")){
+                        VStack{
+                            Picker(selection: $sportCategoryIndex, label: Text("Categrie")){
+                                ForEach(0 ..< sportsCategories.count){
+                                    Text(self.sportsCategories[$0]).tag($0)
+                                }
+                            }.pickerStyle(DefaultPickerStyle())
+                            Picker(selection: $sportCollIndvIndex, label: Text("type")){
+                                ForEach(0 ..< sportsCollIndv.count){
+                                    Text(self.sportsCollIndv[$0]).tag($0)
+                                }
+                            }.pickerStyle(DefaultPickerStyle())
+                        }
+                    }
+                    Section(header: Text("image")){
+                        ZStack {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    image?
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: 150)
+                                    
+                                }
+                                VStack(alignment: .leading){
+                                    Button(action: {
+                                        withAnimation {
+                                            self.isShowPicker.toggle()
+                                        }
+                                    }) {
+                                        Image(systemName: "plus.square")
+                                            .font(.headline)
+                                    }.foregroundColor(.blue)
+                                }
+                            }.sheet(isPresented: $isShowPicker) {
+                                ImagePicker(image: self.$image)
+                            }
+                            
+                            
+                        }
+                    }
+                    
+                    Section {
+                        Button(action: {
+                            print("Perform an action here...")
+                        }) {
+                            Text("Partager le podcast")
+                        }
+                    }
+                }
+                .navigationBarTitle("Proposer un podcast")
             }.tabItem {
-                    Text("Proposer")
-                    Image(systemName: "waveform.path.badge.plus")
+                Text("Proposer")
+                Image(systemName: "waveform.path.badge.plus")
             }
         }
         
         
     }
     
-
+    
 }
 
 struct MultilineTextView: UIViewRepresentable {
     @Binding var text: String
-
+    
     func makeUIView(context: Context) -> UITextView {
         let view = UITextView()
         view.isScrollEnabled = true
         view.isEditable = true
         view.isUserInteractionEnabled = true
+        
         return view
     }
-
+    
     func updateUIView(_ uiView: UITextView, context: Context) {
         uiView.text = text
+        
     }
 }
 
@@ -120,30 +147,27 @@ struct ResultView:View{
     var choice: String;
     
     @State private var sportCollIndvIndex = 0;
-
+    
     var sportsCollIndv = ["Collectif", "Individuel"];
-
+    
     
     var body: some View {
-
-            
-                    VStack{
-                        Picker(selection: $sportCollIndvIndex, label: Text("")){
-                            ForEach(0 ..< sportsCollIndv.count){
-                                Text(self.sportsCollIndv[$0]).tag($0)
-                            }
-                        }.labelsHidden().frame(height: 50.0)
-                            .pickerStyle(SegmentedPickerStyle())
-                            .scaledToFit()
-                            .scaleEffect(CGSize(width: 1.5, height: 1.5))
-                        NavigationLink(destination: PodcastsView(choiceCategorySport: "\(choice)", choiceCollectifIndiv: "\(sportCollIndvIndex)")){
-                            Text("Next >")
-                        }.padding(100)
-                    }
-                    .navigationBarTitle("Trouver votre podcast")
-            
-
-
+        
+        VStack{
+            Picker(selection: $sportCollIndvIndex, label: Text("")){
+                ForEach(0 ..< sportsCollIndv.count){
+                    Text(self.sportsCollIndv[$0]).tag($0)
+                }
+            }.labelsHidden().frame(height: 50.0)
+                .pickerStyle(SegmentedPickerStyle())
+                .scaledToFit()
+                .scaleEffect(CGSize(width: 1.5, height: 1.5))
+            NavigationLink(destination: PodcastsView(choiceCategorySport: "\(choice)", choiceCollectifIndiv: "\(sportCollIndvIndex)")){
+                Text("Next >")
+            }.padding(100)
+        }
+        .navigationBarTitle("Trouver votre podcast")
+        
     }
     
 }
@@ -165,25 +189,25 @@ struct PodcastsView: View {
         case "0":
             
             switch choiceCategorySport {
-                case "0":
-                    return Service.listPhysiqueCollectif
-                    
-                case "1":
-                    return Service.listMentalCollectif;
-                    
-                case "2":
-                    return Service.listVehicCollectif;
-                    
-                case "3":
-                    return Service.listCoordinCollectif;
-                    
-                case "4":
-                    return Service.listAnimauxCollectif;
-                    
-            
-
-                default:
-                    _ = 3;
+            case "0":
+                return Service.listPhysiqueCollectif
+                
+            case "1":
+                return Service.listMentalCollectif;
+                
+            case "2":
+                return Service.listVehicCollectif;
+                
+            case "3":
+                return Service.listCoordinCollectif;
+                
+            case "4":
+                return Service.listAnimauxCollectif;
+                
+                
+                
+            default:
+                _ = 3;
             }
             
             
@@ -191,24 +215,24 @@ struct PodcastsView: View {
         case "1":
             
             switch choiceCategorySport {
-                case "0":
-                    return Service.listPhysiqueIndiv
-                    
-                case "1":
-                    return Service.listMentalIndiv;
-                    
-                case "2":
-                    return Service.listVehicIndiv;
-                    
-                case "3":
-                    return Service.listCoordinIndiv;
-                    
-                case "4":
-                    return Service.listAnimauxIndiv;
-            
-
-                default:
-                    _ = 3;
+            case "0":
+                return Service.listPhysiqueIndiv
+                
+            case "1":
+                return Service.listMentalIndiv;
+                
+            case "2":
+                return Service.listVehicIndiv;
+                
+            case "3":
+                return Service.listCoordinIndiv;
+                
+            case "4":
+                return Service.listAnimauxIndiv;
+                
+                
+            default:
+                _ = 3;
             }
             
             break;
@@ -227,73 +251,73 @@ struct PodcastsView: View {
         //Service.listPodcasts
         
         let b  = List(a) { item in
-                NavigationLink(destination: ListDetailView(item: item)) {
-                    ListElementView(item: item)
-                }
-            }.navigationBarTitle("Meilleurs Podcast")
-            
+            NavigationLink(destination: ListDetailView(item: item)) {
+                ListElementView(item: item)
+            }
+        }.navigationBarTitle("Meilleurs Podcast")
+        
         return b;
-            
         
         
         
-//            Button("Show Alert") {
-//            self.showingAlert = true
-//        }
-//
-//        .alert(isPresented: $showingAlert) {
-//            Alert(title: Text(BoolToString(b: choiceCategorySport == "0")), message: Text(BoolToString(b: choiceCollectifIndiv == "1")), dismissButton: .default(Text("OK")))
-//        }
+        
+        //            Button("Show Alert") {
+        //            self.showingAlert = true
+        //        }
+        //
+        //        .alert(isPresented: $showingAlert) {
+        //            Alert(title: Text(BoolToString(b: choiceCategorySport == "0")), message: Text(BoolToString(b: choiceCollectifIndiv == "1")), dismissButton: .default(Text("OK")))
+        //        }
         
     }
 }
 
 struct ImagePicker: UIViewControllerRepresentable {
-
+    
     @Environment(\.presentationMode)
     var presentationMode
-
+    
     @Binding var image: Image?
-
+    
     class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-
+        
         @Binding var presentationMode: PresentationMode
         @Binding var image: Image?
-
+        
         init(presentationMode: Binding<PresentationMode>, image: Binding<Image?>) {
             _presentationMode = presentationMode
             _image = image
         }
-
+        
         func imagePickerController(_ picker: UIImagePickerController,
                                    didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             let uiImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
             image = Image(uiImage: uiImage)
             presentationMode.dismiss()
-
+            
         }
-
+        
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             presentationMode.dismiss()
         }
-
+        
     }
-
+    
     func makeCoordinator() -> Coordinator {
         return Coordinator(presentationMode: presentationMode, image: $image)
     }
-
+    
     func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
         return picker
     }
-
+    
     func updateUIViewController(_ uiViewController: UIImagePickerController,
                                 context: UIViewControllerRepresentableContext<ImagePicker>) {
-
+        
     }
-
+    
 }
 
 
--- ProjetSwiftUI/ListDetailView.swift
+++ ProjetSwiftUI/ListDetailView.swift
@@ -8,6 +8,34 @@
 
 import SwiftUI
 
+
+struct ImageOverlay: View {
+    var body: some View {
+        ZStack{
+            HStack(alignment: .center){
+                NavigationLink(destination: PodcastPlayer()) {
+                    Button(action: {
+                    }) {
+                        Image(systemName: "play.fill")
+                            .font(.headline)
+                    }.frame(width: 50.0, height: 50.0).foregroundColor(.blue).opacity(0.5)
+                }
+            }
+            
+        }
+//        ZStack {
+//            Text("Credit: Ricardo Gomez Angel")
+//                .font(.callout)
+//                .padding(6)
+//                .foregroundColor(.white)
+//        }.background(Color.black)
+//        .opacity(0.8)
+//        .cornerRadius(10.0)
+//        .padding(6)
+    }
+}
+
+
 struct ListDetailView: View {
     
     var item: ListElement
@@ -23,11 +51,13 @@ struct ListDetailView: View {
                         .font(.largeTitle)
                         .fontWeight(.bold)
                 }.frame(minWidth: 0, maxWidth: .infinity)
-                Image(item.image)
+                HStack{
+                    Image(item.image)
                     .resizable()
                     .renderingMode(.original)
                     .aspectRatio(contentMode: .fit)
-                
+                    .overlay(ImageOverlay(), alignment: .leading)
+                }
                 Text(item.text)
                     .padding(.horizontal, 20)
             }
